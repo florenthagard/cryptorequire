2fd3377b86821ab1e25fb72f01f3cb12806ddbe3
Florent <Florent@MBPFlorent.local>
2016-02-10 14:32:31 +0100

    no message

--- bin/cryptorequire.sh
+++ bin/cryptorequire.sh
@@ -4,6 +4,7 @@ export_directory=$(dirname $(pwd))"/"$(basename $(pwd))".encrypted"
 cd "$homescript/../node_modules/cryptorequire/"
 clear
 npm install --silent;
+echo $source_directory
 pwd
 #rm -rf $export_directory;
 #node encrypt.js -i $source_directory -o $export_directory
