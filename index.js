1b1528a3fda8953e77243a6bcfe3f020465c9b29
Florent <Florent@MBPFlorent.local>
2016-02-12 10:13:40 +0100

    no message

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 require('./lib.js');
 
 module.exports = function(){
-	var __envSecure;
-	prompt.start();
-	prompt.get({properties:{algorithme:{},password:{hidden:true}}},function (err,crypt){
-		try{ 
-			__envSecure = new __envSecure(crypt.algorithme,crypt.password);
-			var decrypted = __envSecure.decrypt(fs.readFileSync(file_context+'/'+file_to_run));
-			eval(decrypted.toString());
-		} catch(e){ console.log(e) }
-	});
+	var crequire = global.__envSecure;
+		  delete   global.__envSecure;
 
-	return __envSecure
+	return {
+		decrypt : function(p){
+			console.log(p,p.match(/\.jse$/gi));
+		}
+	}
 }();
