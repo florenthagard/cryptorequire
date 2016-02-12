34126c2ecba32abbca3cde140bab7ce4c17a8ed6
Florent <Florent@MBPFlorent.local>
2016-02-12 11:07:42 +0100

    no message

--- decrypt.js
+++ decrypt.js
@@ -8,6 +8,11 @@ prompt.get({properties:{algorithme:{},password:{hidden:true}}},function (err,cry
 	try{ 
 		global.__envSecure = new __envSecure(crypt.algorithme,crypt.password);
 		var decrypted      = __envSecure.decrypt(fs.readFileSync(file_context+'/'+file_to_run));
-		eval(decrypted.toString());
+		global.__dirname   = path.dirname(path.normalize(file_context+'/'+file_to_run));
+		global.__filename  = path.basename(path.normalize(file_context+'/'+file_to_run));
+
+		console.log(__dirname,__filename);
+
+		//eval(decrypted.toString());
 	} catch(e){ console.log(e) }
 });
--- lib.js
+++ lib.js
@@ -2,60 +2,6 @@ global.prompt 	= require('prompt');
 global.fs 		= require('fs');
 global.path		= require('path');
 global.crypto 	= require('crypto');
-global.colors 	= require('colors');
-
-Object.defineProperty(Array.prototype, "padl", {
-	enumerable   : false,
-	configurable : true,
-	writable	 : true,
-	value        : function(padnb,pad){
-		var paddArray  = [];
-		var wordLength = 0;
-		for (el in this){
-			if(this[el].length > wordLength){wordLength = this[el].length;}
-		}
-
-		for (el in this){
-			paddArray.push(Array((padnb||wordLength+1)-this[el].length+1).join(pad||' ')+this[el])
-		}
-
-		return paddArray
-	}
-})
-
-Object.defineProperty(Array.prototype, "pagination", {
-	enumerable   : false,
-	configurable : true,
-	writable	 : true,
-	value        : function(modulo){
-		var original = this;
-		var edited = [];
-		while (original.length > 0){
-			edited.push(original.splice(0,modulo||1));
-		}
-		return edited
-	}
-})
-
-Object.defineProperty(Array.prototype, "padr", {
-	enumerable   : false,
-	configurable : true,
-	writable	 : true,
-	value        : function(padnb,pad){
-		var paddArray  = [];
-		var wordLength = 0;
-		for (el in this){
-			if(this[el].length > wordLength){wordLength = this[el].length;}
-		}
-
-		for (el in this){
-			paddArray.push(this[el]+Array((padnb||wordLength)-this[el].length+1).join(pad||' '))
-		}
-
-		return paddArray
-	}
-})
-
 
 global.__envSecure = function(algorithmes,passwd){
     var data 		= {};
@@ -68,26 +14,17 @@ global.__envSecure = function(algorithmes,passwd){
     this.setPasswd(passwd    || 'test');
 }
 
-global.__envSecure.prototype.showCiphersAlgo = function(){
-	var ciphers = crypto.getCiphers().padl();
-		ciphers = ciphers.pagination(Math.floor((process.stdout.columns-18) / (ciphers[0].length)));
-
-		console.log("Voicis une listes des algorithmes disponible : ");
-	for (page in ciphers){
-		console.log(ciphers[page].join("").yellow);
-	}
-}
 global.__envSecure.prototype.encrypt = function(buffer){
 	try{
 		var ect = crypto.createCipher(this.getAlgo(),this.getPasswd());
 		return Buffer.concat([ect.update(buffer),ect.final()]);
-	} catch(e){ this.showCiphersAlgo(); }
+	} catch(e){ console.log(e); }
 }
 global.__envSecure.prototype.decrypt = function (buffer){
 	try{
 		var dct = crypto.createDecipher(this.getAlgo(),this.getPasswd());
 		return Buffer.concat([dct.update(buffer),dct.final()]).toString('utf8');
-	} catch(e){ this.showCiphersAlgo(); }
+	} catch(e){ console.log(e); }
 }
 
 global.getStat = function(path){
