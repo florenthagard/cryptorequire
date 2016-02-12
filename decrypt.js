6c39a2b09cf33b2668faa7e3001dacd8c5c53ef7
Florent <Florent@MBPFlorent.local>
2016-02-12 12:21:21 +0100

    no message

--- decrypt.js
+++ decrypt.js
@@ -8,15 +8,9 @@ prompt.get({properties:{algorithme:{},password:{hidden:true}}},function (err,cry
 	try{ 
 		global.__envSecure = new __envSecure(crypt.algorithme,crypt.password);
 		var decrypted      = __envSecure.decrypt(fs.readFileSync(file_context+'/'+file_to_run));
-		global.__dirname   = path.dirname(path.normalize(file_context+'/'+file_to_run));
-		global.__filename  = path.basename(path.normalize(file_context+'/'+file_to_run));
+			__dirname      = path.dirname(path.normalize(file_context+'/'+file_to_run));
+			__filename     = path.basename(path.normalize(file_context+'/'+file_to_run));
 
-		__dirname   = path.dirname(path.normalize(file_context+'/'+file_to_run));
-		__filename  = path.basename(path.normalize(file_context+'/'+file_to_run));
-
-		console.log(__dirname,__filename);
-		console.log(path.dirname(path.normalize(file_context+'/'+file_to_run)),path.basename(path.normalize(file_context+'/'+file_to_run)));
-
-		//eval(decrypted.toString());
+		eval(decrypted.toString());
 	} catch(e){ console.log(e) }
 });
