3f98f6d356f8c97357c170afad16492965ddbcdf
Florent <Florent@MBPFlorent.local>
2016-02-16 09:54:10 +0100

    no message

--- decrypt.js
+++ decrypt.js
@@ -2,16 +2,15 @@ require('./lib.js');
 
 var file_context = process.argv[2];
 var file_to_run  = process.argv[3];
-var crypt;
 
 prompt.start();
 prompt.get({properties:{algorithme:{},password:{hidden:true}}},function (err,crypt){
 	try{
-		global.crypt	   	 = crypt;
-		global.cryptorequire = new cryptorequire(crypt.algorithme,crypt.password);
-		var decrypted      	 = cryptorequire.decrypt(fs.readFileSync(file_context+'/'+file_to_run));
-			__dirname      	 = path.dirname(path.normalize(file_context+'/'+file_to_run));
-			__filename     	 = path.basename(path.normalize(file_context+'/'+file_to_run));
+
+		global.__cryptorequire = new cryptorequire(crypt.algorithme,crypt.password);
+		var decrypted      	   = cryptorequire.decrypt(fs.readFileSync(file_context+'/'+file_to_run));
+			__dirname      	   = path.dirname(path.normalize(file_context+'/'+file_to_run));
+			__filename     	   = path.basename(path.normalize(file_context+'/'+file_to_run));
 
 		eval(decrypted.toString());
 	} catch(e){ console.log(e) }
