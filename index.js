3511a8892e4bd5323224c5bf56b7cd93d6ac4902
Florent <Florent@MBPFlorent.local>
2016-02-17 12:35:52 +0100

    no message

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 require.extensions['.jse'] = function (module, filename) {
     var content = __cryptorequire.decrypt(fs.readFileSync(filename));
     module._compile(content, filename);
